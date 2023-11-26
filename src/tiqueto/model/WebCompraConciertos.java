be8e50ea951e864e9115c9f4784ecf8b51611a14
Lucía <127886157+luciasimon@users.noreply.github.com>
2023-11-26 16:24:35 +0100

    Update WebCompraConciertos.java

--- src/tiqueto/model/WebCompraConciertos.java
+++ src/tiqueto/model/WebCompraConciertos.java
@@ -9,18 +9,21 @@ public class WebCompraConciertos implements IOperacionesWeb {
         super();
     }
 
+    //Esta variable será la que indicará si la venta se ha cerrado o no
     public AtomicBoolean ventaTerminada = new AtomicBoolean(false);
+    
     int entradasVendidas = 0;
 
     @Override
     public synchronized boolean comprarEntrada() {
-        //quitamos 1 de las entradas totales
+        //Si hay entradas a la venta, compramos una
         if (hayEntradas()) {
             ENTRADAS_DISPONIBLES--;
             entradasVendidas++;
             mensajeWeb("ENTRADA COMPRADA. QUEDAN " + entradasRestantes());
             return true;
         } else {
+            //Si no hay entradas a la venta, esperamos
             try {
                 mensajeWeb("NO HAY ENTRADAS DISPONIBLES. ESPERAMOS.");
                 wait();
@@ -33,7 +36,7 @@ public class WebCompraConciertos implements IOperacionesWeb {
 
     @Override
     public synchronized int reponerEntradas(int numeroEntradas) {
-        //vamos a decrementar el número de entradas que el promotor tiene, e incrementamos las que ponemos a la venta
+        //Para reponer, vamos a añadir a las entradas disponibles el número de entradas que reponemos, que es pasado por parámetros
         ENTRADAS_DISPONIBLES += numeroEntradas;
         notifyAll(); //notificamos a los fans de que ya hay entradas
         return ENTRADAS_DISPONIBLES;
@@ -41,6 +44,7 @@ public class WebCompraConciertos implements IOperacionesWeb {
 
     @Override
     public synchronized void cerrarVenta() {
+        //Al poner la variable como True, estamos cerrando la venta
         ventaTerminada.set(true);
         notifyAll();
         mensajeWeb("VENTA CERRADA");
@@ -50,6 +54,7 @@ public class WebCompraConciertos implements IOperacionesWeb {
     //hay entradas disponibles para compra?
     @Override
     public synchronized boolean hayEntradas() {
+        //Devolverá true si hay entradas disponibles para la venta, si no, devolverá false
         return ENTRADAS_DISPONIBLES > 0;
     }
 
