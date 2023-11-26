d1b62b1efa90819448ca83719c1cb8942aad0ce5
Lucía <127886157+luciasimon@users.noreply.github.com>
2023-11-26 16:28:22 +0100

    Update PromotoraConciertos.java

--- src/tiqueto/model/PromotoraConciertos.java
+++ src/tiqueto/model/PromotoraConciertos.java
@@ -14,11 +14,13 @@ public class PromotoraConciertos extends Thread {
 
     @Override
     public void run() {
+        //Mientras la venta no se haya cerrado...
         while (!webCompra.ventaTerminada.get()) {
             try {
-                //si 1)no hay entradas a la venta, 2) al promotor le quedan entradas, 3)no se ha llegado al máximo que los fans pueden comprar en total...
+                //si 1)no hay entradas a la venta, 2) al promotor le quedan entradas y 3)no se ha llegado al máximo que los fans pueden comprar en total...
                 if (!webCompra.hayEntradas() && webCompra.entradasRestantes() > 0 && (NUM_FANS * MAX_ENTRADAS_POR_FAN) > webCompra.entradasVendidas) {
                     mensajePromotor("Uy, no hay entradas... voy a reponer");
+                    //Si el número de entradas que me quedan es mayor o igual al del número de entradas que quiero reponer, repongo ese número
                     if (webCompra.entradasRestantes() >= REPOSICION_ENTRADAS) {
                         webCompra.reponerEntradas(REPOSICION_ENTRADAS);
                         mensajePromotor("Entradas repuestas (estándar).");
@@ -31,9 +33,10 @@ public class PromotoraConciertos extends Thread {
                     }
                     Thread.sleep(ThreadLocalRandom.current().nextInt(3000, 8000)); //dormimos
 
-                // si 1)no hay entradas a la venta, 2) al promotor le quedan entradas, 3) se ha llegado al máximo que los fans pueden comprar en total...
+                // si 1)no hay entradas a la venta, 2) al promotor le quedan entradas y 3) se ha llegado al máximo que los fans pueden comprar en total...
                 } else if (!webCompra.hayEntradas() && webCompra.entradasRestantes() > 0 && (NUM_FANS * MAX_ENTRADAS_POR_FAN) == webCompra.entradasVendidas) {
                     mensajePromotor("Los fans me han comprado todas las entradas que podían comprar");
+                    //Cerramos la venta porque, aunque al promotor le quedan entradas, los fans ya han llegado a sus máximos
                     webCompra.cerrarVenta();
 
                 //si 1)no hay entradas a la venta, 2) al promotor no le quedan entradas...
